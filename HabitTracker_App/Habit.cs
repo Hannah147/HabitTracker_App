8493a14fc6c6c7fa63e701934cc3984917cb743e
Hannah147 <47416196+Hannah147@users.noreply.github.com>
2019-12-05 18:54:36 +0000

    Add project files.

---
+++ HabitTracker_App/Habit.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HabitTracker_App
+{
+    public class Habit
+    {
+        public string HabitName { get; set; }
+        public double HabitTarget { get; set; }
+        public double HabitProgress { get; set; }
+        public double TotalProgress { get; set; }
+
+        public Habit(string habitName, double habitTarget, double habitProgress, double totalProgress)
+        {
+            HabitName = habitName;
+            HabitTarget = habitTarget;
+            HabitProgress = habitProgress;
+            TotalProgress = totalProgress;
+        }
+
+        public Habit():this("Unknown", 0, 0, 0)
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return $"{HabitName}";
+        }
+    }
+}
---
+++ HabitTracker_App/MainWindow.xaml.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HabitTracker_App
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Habit> allHabits = new List<Habit>();
+        static double total = 0, userInput = 0, finalTotal = 0, target = 0, progress = 0;
+        public static double TotalProgress;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Habit habit1 = new Habit()
+            {
+                HabitName = "Study",
+                HabitTarget = 20000,
+                HabitProgress = 0,
+                TotalProgress = 0
+            };
+
+            Habit habit2 = new Habit()
+            {
+                HabitName = "Assignments",
+                HabitTarget = 40000,
+                HabitProgress = 0,
+                TotalProgress = 0
+            };
+
+            Habit habit3 = new Habit()
+            {
+                HabitName = "Write Notes",
+                HabitTarget = 10000,
+                HabitProgress = 0,
+                TotalProgress = 0
+            };
+
+            Habit habit4 = new Habit()
+            {
+                HabitName = "Attend All Classes",
+                HabitTarget = 30000,
+                HabitProgress = 0,
+                TotalProgress = 0
+            };
+
+            Habit habit5 = new Habit()
+            {
+                HabitName = "Revision",
+                HabitTarget = 50000,
+                HabitProgress = 0,
+                TotalProgress = 0
+            };
+
+            allHabits.Add(habit1);
+            allHabits.Add(habit2);
+            allHabits.Add(habit3);
+            allHabits.Add(habit4);
+            allHabits.Add(habit5);
+
+            lbxAllHabits.ItemsSource = allHabits;
+            userInput = 0;
+        }
+
+        static double addProgress(double _progress)
+        {
+            total = 0;
+            total = total + _progress;
+            return total;
+        }
+
+        public void checkProgress(double target, double finalTotal)
+        {
+            if (finalTotal == target || finalTotal >= target)
+            {
+                tblkAnswer.Text = "You have reached your target!";
+            }
+            else
+            {
+                tblkAnswer.Text = "Keep trying, you're nearly there!";
+            }
+        }
+
+        private void BtnAddProgress_Click(object sender, RoutedEventArgs e)
+        {
+            Habit selectedHabit = lbxAllHabits.SelectedItem as Habit;
+            //userInput = double.Parse(tbxAddProgress.Text);
+            //tblkCurrent.Text = addProgress(userInput).ToString();
+
+            if (selectedHabit != null)
+            {
+
+                userInput = 0;
+                //tbxAddProgress.Clear();
+                tbxHabit.Text = selectedHabit.HabitName;
+                tblkCurrent.Text = selectedHabit.HabitProgress.ToString();
+                tblkTarget.Text = selectedHabit.HabitTarget.ToString();
+
+                selectedHabit.HabitProgress = double.Parse(tbxAddProgress.Text);
+                //tblkCurrent.Text = addProgress(userInput).ToString();
+                selectedHabit.TotalProgress = addProgress(selectedHabit.HabitProgress) + selectedHabit.TotalProgress;
+                //selectedHabit.TotalProgress = selectedHabit.TotalProgress + selectedHabit.HabitProgress;
+                tblkCurrent.Text = selectedHabit.TotalProgress.ToString();
+                checkProgress(selectedHabit.HabitTarget, selectedHabit.TotalProgress);
+
+            }
+
+        }
+
+        private void LbxAllHabits_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Habit selectedHabit = lbxAllHabits.SelectedItem as Habit;
+            if (selectedHabit != null)
+            {
+                tbxAddProgress.Clear();
+                tbxHabit.Text = selectedHabit.HabitName;
+                tblkCurrent.Text = selectedHabit.TotalProgress.ToString();
+                tblkTarget.Text = selectedHabit.HabitTarget.ToString();
+            }
+        }
+    }
+}
